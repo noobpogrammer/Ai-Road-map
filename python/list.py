15d7f28a596e902129ac9d00d5691d09d6b224bb
noobprogrammer <mohammad.esmailjee@gmail.com>
2025-07-23 14:30:01 +0500

    list exercise 3

--- python/list.py
+++ python/list.py
@@ -22,3 +22,13 @@ print(areas_2)
 x = ["a", "b", "c", "d"]
 y = x + ["e", "f"]
 print(y)'''
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Delete the poolhouse items from the list
+del areas[10:12]
+
+# Print the updated list
+print (areas)
