231425f71954e3c6b7643e96606607a846f0b716
noobprogrammer <mohammad.esmailjee@gmail.com>
2025-07-24 08:27:27 +0500

    introduction to packages

---
+++ python/packages.py
@@ -0,0 +1,23 @@
+'''# Import the math package
+import math
+
+# Calculate C
+C = 2 * 0.43 * math.pi
+
+# Calculate A
+A = math.pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
+# Import pi function of math package
+from math import pi
+
+# Calculate C
+C = 2 * 0.43 * pi
+
+# Calculate A
+A = pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))'''
