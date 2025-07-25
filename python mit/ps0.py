a59ac224c5c6be0310b893237b1a90a4942e652b
noobprogrammer <mohammad.esmailjee@gmail.com>
2025-07-25 17:01:53 +0500

    mit python course practice set 0

---
+++ python mit/ps0.py
@@ -0,0 +1,7 @@
+#calculating log of x and x power to y
+#import math laibary for log function
+import math
+x=int(input("enter number x: "))
+y=int(input("enter number y: "))
+print("x**y = ",x**y)
+print("log(x) = ",math.log2(x))
